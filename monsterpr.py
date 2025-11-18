dab14acab887efd3cdb69273b2278c6d5fdb43a3
Alibababeats <ali.salem.03.15@gmail.com>
2025-11-18 11:52:48 -0500

    Add README and initial monsterpr.py file with project description

--- monsterpr.py
+++ monsterpr.py
@@ -9,3 +9,19 @@ A program that acts as like game disguised as a screen saver
 Within the screen saver, there will be a calander, a time, a character
 that either follows the mouse cursor, or controlled by the arrow keys
 """
+import pygame
+
+class Character:
+    pass
+
+class DateDisplay:
+    pass
+
+class TimeDisplay:
+    pass
+
+class QuoteDisplay:
+    pass
+
+class Background:
+    pass
