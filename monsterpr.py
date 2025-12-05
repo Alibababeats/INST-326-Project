831f84ebc0592f7db3541df3eeedfa17af0d3d08
Alibababeats <ali.salem.03.15@gmail.com>
2025-12-05 10:16:06 -0500

    changed format from iterating daily to every 4 hours

--- monsterpr.py
+++ monsterpr.py
@@ -8,6 +8,8 @@ A program that acts as like game disguised as a screen saver
 
 Within the screen saver, there will be a calander, a time, a character
 that either follows the mouse cursor, or controlled by the arrow keys
+
+Background iterates through different backgrounds every 4 hours
 """
 import pygame
 import os 
@@ -40,13 +42,13 @@ class TimeDisplay:
 
 class QuoteOfTheDayDisplay:
     """
-    Displays the motivational quote on the screen and changes each day.
+    Displays the motivational quote on the screen and changes with the background every 4 hours.
     """
     pass
 
 class Background:
     """
-    Manages the background images and changes them each day.
+    Manages the background images and changes them every 4 hours.
     """
     pass
 
